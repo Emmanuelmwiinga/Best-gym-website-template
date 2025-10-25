2f2fe4bcb97d05fed15190a83194df8cc49c1c5b
Emmanuel.dev <Emmanuelmwiingaixii@gmail.com>
2025-10-25 07:40:00 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
   smooth: true,
   lerp: 0.1,
-  multiplier: 3.0,
+  multiplier: 2.0,
   smartphone: { smooth: true },
   tablet: { smooth: true }
 });
